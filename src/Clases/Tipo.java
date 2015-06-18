b7c7ef6fa09a0700c4671b57a1c7e8016d6b449b
Jhumari Soto <Jhumari Soto@TOSHIBA>
2015-06-17 20:32:40 -0500

    mi primer comit

---
+++ src/Clases/Tipo.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Jhumari Soto
+ */
+public enum Tipo {
+    canarios ("canarios",Especie.aves),
+    pelicanos("pelicanos",Especie.aves),
+    pinguinos("pinguinos",Especie.aves),
+    patos("patos",Especie.aves),
+    gallina("gallina",Especie.aves),
+    turacus("turacus",Especie.aves),
+    lechusas("lechusa",Especie.aves),
+    palomas("palomas",Especie.aves),
+    aguilas("aguilas",Especie.aves),
+    loros("loros",Especie.aves),
+    gaviotas("gaviotas",Especie.aves),
+    garsas("garsas",Especie.aves),
+    halcones("halcones",Especie.aves),
+    piguichu("piguichu",Especie.aves),
+    pavos("pavos",Especie.aves),
+    perros("perros",Especie.Mamiferos),
+    monos("monos",Especie.Mamiferos),
+    vacas("vacas",Especie.Mamiferos),
+    gatos("gatos",Especie.Mamiferos),
+    conejos("conejos",Especie.Mamiferos),
+    Higuanas("Higuanas",Especie.Mamiferos),
+    caballos("caballos",Especie.Mamiferos),
+    cuyes("cuyes",Especie.Mamiferos),
+    ardillas("ardillas",Especie.Mamiferos),
+    llamas("llamas",Especie.Mamiferos),
+    ovejas("ovejas",Especie.Mamiferos),
+    chivos("chivos",Especie.Mamiferos);
+   
+    
+   private final String nombre;
+   private final Enum especie;
+
+    private Tipo(String nombre, Enum especie){
+        this.nombre = nombre;
+        this.especie = especie;
+    }
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Enum getEspecie() {
+        return especie;
+    }
+   
+    
+    
+    
+    
+    
+    
+            
+    
+}
